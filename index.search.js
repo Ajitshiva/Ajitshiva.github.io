dbfb9e2e3d54fa855df52dad80bb778fe36a1f47
Ajitshiva <ajitshiva007@gmail.com>
2024-02-25 20:17:20 +0530

    remove giscus

--- index.search.js
+++ index.search.js
@@ -9,7 +9,7 @@ var relearn_search_index = [
   },
   {
     "breadcrumb": "Kode Kaavalan \u003e Tutorials \u003e Data Structures",
-    "content": "Advanced\n",
+    "content": "\r{{ range $number := (seq 10) }}\r{{ $number }}\r{{ end }}\r",
     "description": "",
     "tags": null,
     "title": "Advanced",
@@ -25,7 +25,7 @@ var relearn_search_index = [
   },
   {
     "breadcrumb": "Kode Kaavalan \u003e Tutorials",
-    "content": "",
+    "content": " Dynamic Programming Graph Theory Mathematical Sorting Searching ",
     "description": "",
     "tags": null,
     "title": "Algorithms",
@@ -89,11 +89,27 @@ var relearn_search_index = [
   },
   {
     "breadcrumb": "Kode Kaavalan \u003e Tutorials",
-    "content": "",
+    "content": " Basic Intermediate Advanced ",
     "description": "",
     "tags": null,
     "title": "Data Structures",
     "uri": "/tutorials/data_structures/index.html"
+  },
+  {
+    "breadcrumb": "Kode Kaavalan \u003e Tutorials \u003e Algorithms",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Dynamic Programming",
+    "uri": "/tutorials/algorithms/dynamic_programming/index.html"
+  },
+  {
+    "breadcrumb": "Kode Kaavalan \u003e Tutorials \u003e Algorithms",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Graph Theory",
+    "uri": "/tutorials/algorithms/graph_theory/index.html"
   },
   {
     "breadcrumb": "Kode Kaavalan \u003e Tutorials \u003e Data Structures",
@@ -113,7 +129,7 @@ var relearn_search_index = [
   },
   {
     "breadcrumb": "",
-    "content": "Are you passionate about programming, data structures, algorithms, and competitive coding? Look no further! Kodu Kaavalan is your go-to destination for all things related to mastering programming challenges and honing your problem-solving skills.\nWhat Sets Us Apart Algorithmic Insights: Explore in-depth explanations and intuitive breakdowns of algorithmic concepts ranging from classic sorting and searching techniques to dynamic programming, graph algorithms, and beyond. Our blog is your gateway to understanding the intricate logic behind these algorithms, empowering you to tackle complex coding challenges with confidence.\nProgramming Contest Problems: Immerse yourself in a treasure trove of programming contest problems curated from leading platforms such as Codeforces, Codechef, LeetCode, and AtCoder. Each problem comes with a detailed analysis, providing valuable insights into problem-solving strategies and algorithmic approaches.\nComprehensive Tutorials: Whether you’re a beginner or an experienced coder, our comprehensive tutorials cater to all skill levels. From foundational concepts to advanced techniques, we cover everything you need to know to excel in competitive programming and beyond.\nInteractive Learning Experience: Engage with our content through interactive code examples, visualizations, and real-world applications. We believe in learning by doing, and our hands-on approach ensures that you not only understand algorithms and data structures but also how to apply them effectively in practice.\nThriving Community: Join our vibrant community of programming enthusiasts, where you can connect with like-minded individuals, share insights, and collaborate on solving challenging problems. Whether you’re seeking advice, discussing solutions, or simply celebrating your coding victories, you’ll find a supportive network ready to cheer you on.\nEmbark on Your Coding Journey Today!\nReady to embark on an exciting journey through the realms of algorithms, data structures, and competitive coding? Dive into our latest posts, tutorials, and problem-solving guides to elevate your programming skills to new heights. Whether you’re aiming for success in coding interviews, competitions, or personal projects, [Your Blog Name] is your trusted companion every step of the way.\nLet’s unravel the mysteries of algorithms, one problem at a time. Welcome to a world of endless possibilities, where curiosity meets creativity, and challenges become opportunities for growth. Join us on [Your Blog Name] and start coding your way to success!\n",
+    "content": "Are you passionate about programming, data structures, algorithms, and competitive coding? Look no further! Kode Kaavalan is your go-to destination for all things related to mastering programming challenges and honing your problem-solving skills.\nWhat Sets Us Apart Algorithmic Insights: Explore in-depth explanations and intuitive breakdowns of algorithmic concepts ranging from classic sorting and searching techniques to dynamic programming, graph algorithms, and beyond. Our blog is your gateway to understanding the intricate logic behind these algorithms, empowering you to tackle complex coding challenges with confidence.\nProgramming Contest Problems: Immerse yourself in a treasure trove of programming contest problems curated from leading platforms such as Codeforces, Codechef, LeetCode, and AtCoder. Each problem comes with a detailed analysis, providing valuable insights into problem-solving strategies and algorithmic approaches.\nComprehensive Tutorials: Whether you’re a beginner or an experienced coder, our comprehensive tutorials cater to all skill levels. From foundational concepts to advanced techniques, we cover everything you need to know to excel in competitive programming and beyond.\nInteractive Learning Experience: Engage with our content through interactive code examples, visualizations, and real-world applications. We believe in learning by doing, and our hands-on approach ensures that you not only understand algorithms and data structures but also how to apply them effectively in practice.\nThriving Community: Join our vibrant community of programming enthusiasts, where you can connect with like-minded individuals, share insights, and collaborate on solving challenging problems. Whether you’re seeking advice, discussing solutions, or simply celebrating your coding victories, you’ll find a supportive network ready to cheer you on.\nEmbark on Your Coding Journey Today!\nReady to embark on an exciting journey through the realms of algorithms, data structures, and competitive coding? Dive into our latest posts, tutorials, and problem-solving guides to elevate your programming skills to new heights. Whether you’re aiming for success in coding interviews, competitions, or personal projects, [Your Blog Name] is your trusted companion every step of the way.\nLet’s unravel the mysteries of algorithms, one problem at a time. Welcome to a world of endless possibilities, where curiosity meets creativity, and challenges become opportunities for growth. Join us on [Your Blog Name] and start coding your way to success!\n",
     "description": "",
     "tags": null,
     "title": "Kode Kaavalan",
@@ -128,12 +144,28 @@ var relearn_search_index = [
     "uri": "/leetcode/index.html"
   },
   {
+    "breadcrumb": "Kode Kaavalan \u003e Tutorials \u003e Algorithms",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Mathematical",
+    "uri": "/tutorials/algorithms/mathematical/index.html"
+  },
+  {
     "breadcrumb": "Kode Kaavalan \u003e CodeForces",
     "content": "",
     "description": "",
     "tags": null,
     "title": "Rounds",
     "uri": "/codeforces/rounds/index.html"
+  },
+  {
+    "breadcrumb": "Kode Kaavalan \u003e Tutorials \u003e Algorithms",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Sorting Searching",
+    "uri": "/tutorials/algorithms/sorting_searching/index.html"
   },
   {
     "breadcrumb": "Kode Kaavalan",
