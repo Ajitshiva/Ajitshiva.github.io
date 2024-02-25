0315cc7f29d5b96dcb5693040fa4857cf1b612a2
Ajitshiva <ajitshiva007@gmail.com>
2024-02-25 18:59:39 +0530

    Init folder Structure

--- index.search.js
+++ index.search.js
@@ -1,6 +1,54 @@
 var relearn_search_index = [
   {
-    "breadcrumb": "Kodu Kaavalan",
+    "breadcrumb": "Kode Kaavalan \u003e Atcoder",
+    "content": "abc\n",
+    "description": "",
+    "tags": null,
+    "title": "abc",
+    "uri": "/atcoder/abc/index.html"
+  },
+  {
+    "breadcrumb": "Kode Kaavalan \u003e Tutorials \u003e Data Structures",
+    "content": "Advanced\n",
+    "description": "",
+    "tags": null,
+    "title": "Advanced",
+    "uri": "/tutorials/data_structures/advanced/index.html"
+  },
+  {
+    "breadcrumb": "Kode Kaavalan \u003e Interview Preparation \u003e Topic Wise",
+    "content": "Algorithms\n",
+    "description": "",
+    "tags": null,
+    "title": "Algorithms",
+    "uri": "/interview_preparation/topic_wise/algorithms/index.html"
+  },
+  {
+    "breadcrumb": "Kode Kaavalan \u003e Tutorials",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Algorithms",
+    "uri": "/tutorials/algorithms/index.html"
+  },
+  {
+    "breadcrumb": "Kode Kaavalan",
+    "content": "Atcoder\n",
+    "description": "",
+    "tags": null,
+    "title": "Atcoder",
+    "uri": "/atcoder/index.html"
+  },
+  {
+    "breadcrumb": "Kode Kaavalan \u003e Tutorials \u003e Data Structures",
+    "content": "Basic\n",
+    "description": "",
+    "tags": null,
+    "title": "Basic",
+    "uri": "/tutorials/data_structures/basic/index.html"
+  },
+  {
+    "breadcrumb": "Kode Kaavalan",
     "content": "",
     "description": "",
     "tags": null,
@@ -8,19 +56,107 @@ var relearn_search_index = [
     "uri": "/categories/index.html"
   },
   {
+    "breadcrumb": "Kode Kaavalan",
+    "content": "Challenges\n",
+    "description": "",
+    "tags": null,
+    "title": "Challenges",
+    "uri": "/challenges/index.html"
+  },
+  {
+    "breadcrumb": "Kode Kaavalan",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "CodeChef",
+    "uri": "/codechef/index.html"
+  },
+  {
+    "breadcrumb": "Kode Kaavalan",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "CodeForces",
+    "uri": "/codeforces/index.html"
+  },
+  {
+    "breadcrumb": "Kode Kaavalan \u003e Interview Preparation \u003e Topic Wise",
+    "content": "Data Structures\n",
+    "description": "",
+    "tags": null,
+    "title": "Data Structures",
+    "uri": "/interview_preparation/topic_wise/data_structures/index.html"
+  },
+  {
+    "breadcrumb": "Kode Kaavalan \u003e Tutorials",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Data Structures",
+    "uri": "/tutorials/data_structures/index.html"
+  },
+  {
+    "breadcrumb": "Kode Kaavalan \u003e Tutorials \u003e Data Structures",
+    "content": "Intermediate\n",
+    "description": "",
+    "tags": null,
+    "title": "Intermediate",
+    "uri": "/tutorials/data_structures/intermediate/index.html"
+  },
+  {
+    "breadcrumb": "Kode Kaavalan",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Interview Preparation",
+    "uri": "/interview_preparation/index.html"
+  },
+  {
     "breadcrumb": "",
     "content": "Are you passionate about programming, data structures, algorithms, and competitive coding? Look no further! Kodu Kaavalan is your go-to destination for all things related to mastering programming challenges and honing your problem-solving skills.\nWhat Sets Us Apart Algorithmic Insights: Explore in-depth explanations and intuitive breakdowns of algorithmic concepts ranging from classic sorting and searching techniques to dynamic programming, graph algorithms, and beyond. Our blog is your gateway to understanding the intricate logic behind these algorithms, empowering you to tackle complex coding challenges with confidence.\nProgramming Contest Problems: Immerse yourself in a treasure trove of programming contest problems curated from leading platforms such as Codeforces, Codechef, LeetCode, and AtCoder. Each problem comes with a detailed analysis, providing valuable insights into problem-solving strategies and algorithmic approaches.\nComprehensive Tutorials: Whether you’re a beginner or an experienced coder, our comprehensive tutorials cater to all skill levels. From foundational concepts to advanced techniques, we cover everything you need to know to excel in competitive programming and beyond.\nInteractive Learning Experience: Engage with our content through interactive code examples, visualizations, and real-world applications. We believe in learning by doing, and our hands-on approach ensures that you not only understand algorithms and data structures but also how to apply them effectively in practice.\nThriving Community: Join our vibrant community of programming enthusiasts, where you can connect with like-minded individuals, share insights, and collaborate on solving challenging problems. Whether you’re seeking advice, discussing solutions, or simply celebrating your coding victories, you’ll find a supportive network ready to cheer you on.\nEmbark on Your Coding Journey Today!\nReady to embark on an exciting journey through the realms of algorithms, data structures, and competitive coding? Dive into our latest posts, tutorials, and problem-solving guides to elevate your programming skills to new heights. Whether you’re aiming for success in coding interviews, competitions, or personal projects, [Your Blog Name] is your trusted companion every step of the way.\nLet’s unravel the mysteries of algorithms, one problem at a time. Welcome to a world of endless possibilities, where curiosity meets creativity, and challenges become opportunities for growth. Join us on [Your Blog Name] and start coding your way to success!\n",
     "description": "",
     "tags": null,
-    "title": "Kodu Kaavalan",
+    "title": "Kode Kaavalan",
     "uri": "/index.html"
   },
   {
-    "breadcrumb": "Kodu Kaavalan",
+    "breadcrumb": "Kode Kaavalan",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "LeetCode",
+    "uri": "/leetcode/index.html"
+  },
+  {
+    "breadcrumb": "Kode Kaavalan \u003e CodeForces",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Rounds",
+    "uri": "/codeforces/rounds/index.html"
+  },
+  {
+    "breadcrumb": "Kode Kaavalan",
     "content": "",
     "description": "",
     "tags": null,
     "title": "Tags",
     "uri": "/tags/index.html"
+  },
+  {
+    "breadcrumb": "Kode Kaavalan \u003e Interview Preparation",
+    "content": "Topic Wise\n",
+    "description": "",
+    "tags": null,
+    "title": "Topic Wise",
+    "uri": "/interview_preparation/topic_wise/index.html"
+  },
+  {
+    "breadcrumb": "Kode Kaavalan",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tutorials",
+    "uri": "/tutorials/index.html"
   }
 ]
