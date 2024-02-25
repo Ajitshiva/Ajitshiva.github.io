6a478011048b4c39c6a1dee6d87f170552f07ca1
Ajitshiva <ajitshiva007@gmail.com>
2024-02-25 12:15:21 +0530

    Initial Commit

---
+++ index.search.js
@@ -0,0 +1,34 @@
+var relearn_search_index = [
+  {
+    "breadcrumb": "Data Structures And Algorithms",
+    "content": "print(\"ajit\")",
+    "description": "",
+    "tags": null,
+    "title": "Basics",
+    "uri": "/basics/index.html"
+  },
+  {
+    "breadcrumb": "Data Structures And Algorithms",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Data Structures And Algorithms",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "Data Structures And Algorithms",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
